37954d7ccff6eb95c88b68ef943959a1d154794b
balajisk12 <balajisk.it22@bitsathy.ac.in>
2025-04-14 11:09:05 +0530

    Initial commit

---
+++ src/App.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+
+
+
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+       
+        int boardwidth = 600;
+        int boardheight=boardwidth;
+
+        JFrame frame =new JFrame("Snake");
+
+        frame.setVisible(true);
+        frame.setSize(boardwidth,boardheight);
+        frame.setLocationRelativeTo(null); 
+        frame.setResizable(false); 
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+
+    }
+}
