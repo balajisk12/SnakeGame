a971737f57f31ea163ef03b55531cc527232be78
BALAJI S <136877298+balajisk12@users.noreply.github.com>
2025-04-27 11:05:34 +0530

    Create class Structure for Tile

---
+++ src/SnakeGame.java
@@ -0,0 +1,19 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.ArrayList;
+import java.util.Random;
+import javax.swing.*;
+
+public class SnakeGame extends JPanel implements ActionListener, KeyListener {
+
+    private class Tile {
+        int x;
+        int y;
+
+        Tile(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
+
