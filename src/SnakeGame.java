0555d7ba3cb8ff3118abf9e8a843037c7f9fe024
BALAJI S <136877298+balajisk12@users.noreply.github.com>
2025-05-06 21:38:12 +0530

    Define Board and Tile Properties, Constructor Basic Setup

--- src/SnakeGame.java
+++ src/SnakeGame.java
@@ -15,5 +15,41 @@ public class SnakeGame extends JPanel implements ActionListener, KeyListener {
             this.y = y;
         }
     }
+
+    int boardwidth;
+    int boardheight;
+    int tilesize = 25;
+
+    Tile snakehead;
+    ArrayList<Tile> snakeBody;
+
+    Tile food;
+    Random random;
+
+    Timer gameloop;
+    int velocityx;
+    int velocityy;
+
+    boolean gameover = false;
+
+    SnakeGame(int boardwidth, int boardheight) {
+        this.boardwidth = boardwidth;
+        this.boardheight = boardheight;
+
+        setPreferredSize(new Dimension(this.boardwidth, this.boardheight));
+        setBackground(Color.black);
+        addKeyListener(this);
+        setFocusable(true);
+
+        snakehead = new Tile(5, 5);
+        snakeBody = new ArrayList<Tile>();
+
+        food = new Tile(10, 10);
+        random = new Random();
+
+        velocityx = 0;
+        velocityy = 0;
+    }
 }
+
 
